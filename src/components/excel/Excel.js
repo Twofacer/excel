1fd1c4062190c559b3dfebc31261749a6ecdc9c5
Vitaly Telpish <viocar112@gmail.com>
2020-07-14 12:34:02 +0300

    Add domeventlistener

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,22 +2,24 @@ import {$} from '@core/dom'
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
   getRoot() {
     const $root = $.create('div', 'excel')
-    $root.classList.add('excel')
+    // $root.classList.add('excel')
 
-    this.components.forEach( Component => {
+    this.components = this.components.map( Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
+      return component
     })
     return $root
   }
   render() {
-    this.$el.append(this.getRoot())
+    this.$el.append(this.getRoot().$el)
+    this.components.forEach(component => component.init())
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,39 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string' ?
+     document.querySelector(selector) :
+     selector
+  }
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    }
+    return this.$el.outerHTMl.trim()
+  }
 
+  clear() {
+    this.html('')
+    return this
+  }
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback)
+  }
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+    if (Element.prototype.append) {
+      this.$el.append(node)
+    } else {
+      this.$el.appendChild(node)
+    }
+    return this
+  }
 }
 
-export function $() {
-  return new Dom()
+export function $(selector) {
+  return new Dom(selector)
 }
 
 $.create = (tagname, classes) => {
@@ -11,5 +41,5 @@ $.create = (tagname, classes) => {
   if (classes) {
     el.classList.add(classes)
   }
-  return el
+  return $(el)
 }
