ec95427ac4978b83df005559018f929b331a3991
Vitaly Telpish <viocar112@gmail.com>
2020-07-16 15:42:24 +0300

    Start resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,29 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template'
+import {$} from '@core/dom'
+
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
+    constructor($root) {
+      super($root, {
+        listeners: ['mousedown']
+      })
+    }
     toHTML() {
-      return createTable()
+      return createTable(50)
+    }
+
+    onMousedown(event) {
+      if (event.target.dataset.resize) {
+        const $resizer = $(event.target)
+        const $parent = $resizer.closest('[data-type="resizable"]')
+        console.log($parent.getCoords())
+
+        document.onmousemove = e => {
+
+        }
+      }
     }
 }
