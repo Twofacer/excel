4e519eafadc2f6b78f72c760f429bf960e455e57
Vitaly Telpish <viocar112@gmail.com>
2020-07-18 18:28:33 +0300

    End resize table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,46 +1,26 @@
-import {ExcelComponent} from '@core/ExcelComponent';
+import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
-import {$} from '@core/dom'
-
+import {resizeHandler} from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.function'
 
 export class Table extends ExcelComponent {
-    static className = 'excel__table'
+  static className = 'excel__table'
 
-    constructor($root) {
-      super($root, {
-        listeners: ['mousedown']
-      })
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
+  toHTML() {
+    return createTable(40)
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
-    toHTML() {
-      return createTable(50)
-    }
-
-    onMousedown(event) {
-      if (event.target.dataset.resize) {
-        const $resizer = $(event.target)
-        const $parent = $resizer.closest('[data-type="resizable"]')
-        const coords = $parent.getCoords()
-        const type = $resizer.data.resize
-        console.log(type)
-        const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-
-        document.onmousemove = e => {
-          if (type === 'col') {
-            const delta = e.pageX - coords.right
-            const value = coords.width + delta
-            $parent.css({width: value + 'px'})
-            cells.forEach(el => el.style.width = value + 'px')
-          } else {
-            const delta = e.pageY - coords.bottom
-            console.log(delta)
-            const value = coords.height + delta
-            $parent.css({height: value + 'px'})
-          }
-        }
-
-        document.onmouseup = () => {
-          document.onmousemove = null
-        }
-      }
-    }
+  }
 }
+
+
