5e5f8aabfc4aba23307585cb98d375bc0366e0de
Vitaly Telpish <viocar112@gmail.com>
2020-07-19 22:14:22 +0300

    Add selecte function

--- src/core/dom.js
+++ src/core/dom.js
@@ -24,6 +24,10 @@ class Dom {
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
   }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
   append(node) {
     if (node instanceof Dom) {
       node = node.$el
@@ -54,6 +58,13 @@ class Dom {
 
   css(styles ={}) {
     Object.keys(styles).forEach( key => this.$el.style[key] = styles[key])
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className)
+  }
+  removeClass(className) {
+    this.$el.classList.remove(className)
   }
 }
 
