bb0f45f32e3781a60ff9644bfd35c67473a12c18
Vitaly Telpish <viocar112@gmail.com>
2020-07-20 17:02:51 +0300

    Button navigation

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,17 +1,17 @@
-import {$} from '@core/dom'
 import {ExcelComponent} from '@core/ExcelComponent'
+import {$} from '@core/dom'
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
-import {shouldResize} from '@/components/table/table.function'
-import {TableSelection} from '@/components/table//TableSelection'
-import {isCell} from '@/components/table/table.function'
+import {isCell,
+  matrix, nextSelector, shouldResize} from '@/components/table/table.function'
+import {TableSelection} from '@/components/table/TableSelection'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown']
+      listeners: ['mousedown', 'keydown']
     })
   }
 
@@ -20,22 +20,48 @@ export class Table extends ExcelComponent {
   }
 
   prepare() {
-    console.log
+    this.selection = new TableSelection()
   }
+
   init() {
     super.init()
-    this.seletion = new TableSelection
+
     const $cell = this.$root.find('[data-id="0:0"]')
-    this.seletion.select($cell)
+    this.selection.select($cell)
   }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event)
     } else if (isCell(event)) {
       const $target = $(event.target)
-      this.seletion.select($target)
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+            .map(id => this.$root.find(`[data-id="${id}"]`))
+        this.selection.selectGroup($cells)
+      } else {
+        this.selection.select($target)
+      }
+    }
+  }
+
+  onKeydown(event) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowDown',
+      'ArrowUp'
+    ]
+
+    const {key} = event
+
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault()
+      const id = this.selection.current.id(true)
+      const $next = this.$root.find(nextSelector(key, id))
+      this.selection.select($next)
     }
   }
 }
-
-
--- src/core/dom.js
+++ src/core/dom.js
@@ -60,6 +60,22 @@ class Dom {
     Object.keys(styles).forEach( key => this.$el.style[key] = styles[key])
   }
 
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
   addClass(className) {
     this.$el.classList.add(className)
   }
